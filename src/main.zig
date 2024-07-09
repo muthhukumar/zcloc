649ec6532fa0539550310c9059a02b8887c89ee5
muthhukumar <muthukumarperiyasamy.mk@gmail.com>
2024-07-09 04:30:19 -0700

    read the args and read the files and count lines of that files is working now

--- src/main.zig
+++ src/main.zig
@@ -3,21 +3,63 @@ const print = std.debug.print;
 const parseInt = std.fmt.parseInt;
 
 pub fn main() !void {
-    const result = try read_file_lines("./output.txt");
-
-    print("num of lines in {}{}\n", .{ result[0], result[1] });
-}
-
-fn read_file_lines(file_path: []const u8) !struct { i64, u64 } {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
+    const arg = try readArgs(allocator);
+
+    if (arg.?.len == 0) {
+        // TODO: Stop the program
+        return;
+    }
+
+    const dir = arg.?;
+
+    var currDir = try std.fs.cwd().openDir(dir, .{ .iterate = true });
+    defer currDir.close();
+
+    var iterator = currDir.iterate();
+
+    while (try iterator.next()) |entry| {
+        const file_name = try std.fmt.allocPrint(allocator, "{s}/{s}", .{ dir, entry.name });
+        defer allocator.free(file_name);
+
+        const stat = try std.fs.cwd().statFile(file_name);
+
+        if (stat.kind != .file) {
+            print("'{s}' is not a file so ignoring it.\n", .{entry.name});
+            continue;
+        }
+
+        const count = try readFileLines(file_name, allocator);
+
+        print("{s} = {d} Lines, {d} Characters\n", .{ entry.name, count[0], count[1] });
+        print("\n", .{});
+    }
+}
+
+fn readArgs(allocator: std.mem.Allocator) !?[]const u8 {
+    var iterator = try std.process.argsWithAllocator(allocator);
+    defer iterator.deinit();
+
+    _ = iterator.next();
+
+    if (iterator.next()) |currPath| {
+        return currPath;
+    }
+
+    return undefined;
+}
+
+fn readDirRecursively(dirPath: []const u8, allocator: std.mem.Allocator, lines: *i64) !void {}
+
+fn readFileLines(file_path: []const u8, allocator: std.mem.Allocator) !struct { i64, u64 } {
     const file = try std.fs.cwd().openFile(file_path, .{});
     defer file.close();
 
     const file_size = (try file.stat()).size;
-    const buffer = try allocator.alloc(u8, file_size + 1);
+    const buffer = try allocator.alloc(u8, file_size);
     defer allocator.free(buffer);
 
     var count: i64 = 0;
@@ -26,8 +68,6 @@ fn read_file_lines(file_path: []const u8) !struct { i64, u64 } {
     while (try file.reader().readUntilDelimiterOrEof(buffer, '\n')) |line| {
         count += 1;
         chars += line.len;
-
-        print("{s}\n", .{line});
     }
 
     return .{ count, chars };
